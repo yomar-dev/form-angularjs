eaf1cf6ef157eeb7ff181c6db2603283c59ee07e
Yomar Miranda <mirandayomar@gmail.com>
2018-09-09 17:02:51 -0500

    Custom error messages.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
-
 var app = angular.module('formApp',['jcs-autoValidate']);
+
+angular.module('formApp')
+    .run([
+        'defaultErrorMessageResolver',
+        function(defaultErrorMessageResolver){
+            defaultErrorMessageResolver.getErrorMessages().then(function(errorMessages){
+                errorMessages['minAge'] = 'The minimun age is {0} years';
+                errorMessages['maxAge'] = 'The maximun age is {0} years';
+            });
+        }
+    ]);
 
 app.controller('mainCtrl', ['$scope', function($scope){
     $scope.formData = {};
     $scope.save = function(valid){
-        if(!valid){
-            return;
-        }
+        console.log('Ok')
     }
 }]);
