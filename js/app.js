300e74a7c44d597a390426b0654d77d167eb0935
Yomar Miranda <mirandayomar@gmail.com>
2018-09-09 16:32:49 -0500

    Validate form manually.

--- js/app.js
+++ js/app.js
@@ -2,4 +2,10 @@
 var app = angular.module('formApp',[ ]);
 
 app.controller('mainCtrl', ['$scope', function($scope){
+    $scope.formData = {};
+    $scope.save = function(valid){
+        if(!valid){
+            return;
+        }
+    }
 }]);
